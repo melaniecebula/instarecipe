1f4c5a2e2fb349b87b70403a1951af4c5aea233a
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-09-28 19:56:03 -0700

    ~ move instagram loader to backend

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 var express = require('express');
+var request = require('request');
 var app = express();
+
 app.configure(function() {
-    app.use(express.compress());
-    app.set("views", __dirname + "/views");
-    app.set("view engine", "ejs");
-    app.use(express["static"] (__dirname + "/static"));
-    app.use(app.router);
+  app.use(express.compress());
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "ejs");
+  app.use(express["static"] (__dirname + "/static"));
+  app.use(app.router);
 });
+
 app.get("/", function(req,res) {
-    res.render("index.ejs");
+  url = 'https://api.instagram.com/v1/tags/recipeshare/media/recent?client_id=452916002&access_token=' + process.env.INSTAGRAM_TOKEN;
+  request.get(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      data = JSON.parse(body);
+      links = [];
+      images = [];
+      console.log(data);
+      for (var i = 0; i < 20; i++) {
+        links.push(data.data[i].link);
+        images.push(data.data[i].images.low_resolution.url);
+      }
+
+      console.log(links);
+      console.log(images);
+
+      res.render("index.ejs", {links: links, images: images});
+    } else {
+      console.log(error);
+    }
+  });
 });
+
 app.listen(8000);
 console.log('Listening on port 8000');
